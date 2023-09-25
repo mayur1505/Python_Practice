773fa0f148ba7cdc24585f42351dacff4125298e
mayurshimpi <mayurshimpi2014@gmail.com>
2023-09-25 19:02:40 +0530

    made folders

---
+++ basics/while.py
@@ -0,0 +1,8 @@
+#while statment is similar to "for" statement with little difference in syntax
+
+# print 1 to 5 using while loop
+
+i=1
+while i<=5:
+    print(i)
+    i=i+1 #incrementing the counter is imp thing is while loop
