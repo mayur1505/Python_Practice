007c6a1846b691301f8ee4d06d0b7452dce4fe58
mayurshimpi <mayurshimpi2014@gmail.com>
2023-10-02 18:19:56 +0530

    new

---
+++ json/json1.py
@@ -0,0 +1,25 @@
+#working with JSON objets
+#JSON is Java Script Object Notation
+#JSON is a data exchange format similat to XML
+#example of JSON and XML
+#Below one is JSON
+# {"employees":[
+#   { "firstName":"John", "lastName":"Doe" },
+#   { "firstName":"Anna", "lastName":"Smith" },
+#   { "firstName":"Peter", "lastName":"Jones" }
+# ]}
+#Below one is xml
+# <employees>
+#   <employee>
+#     <firstName>John</firstName> <lastName>Doe</lastName>
+#   </employee>
+#   <employee>
+#     <firstName>Anna</firstName> <lastName>Smith</lastName>
+#   </employee>
+#   <employee>
+#     <firstName>Peter</firstName> <lastName>Jones</lastName>
+#   </employee>
+# </employees>
+
+#JSON is lightweight format compared to xml
+
---
+++ json/json2.py
@@ -0,0 +1,34 @@
+#we will write two programs
+#1) to create address book and write some records into it
+#2) read this address book
+
+#There is no object called JSON in python.
+#Pythn's native objects are numbers, strings, dictinoaries etc.
+#JSON is just a concept and format, which is implemented by different languages. So all languages
+#like c++, javascript, python supports it and it is a very generic concept
+#Whereas dictinoary is only specific to python
+
+
+#so we will make a dictinoary first
+book={} #book is a dictinoary
+book['ram']={
+    'name':'ram',
+    'address':'murgesh paliya, sector 1',
+    'phone':8388438438
+}
+book['sham']={
+    'name':'sham',
+    'address':'whitefield, sector 3',
+    'phone':62628438438
+}
+
+
+import json
+
+s=json.dumps(book) #it is taking dictionary object book as an input and it is dumping it as a string
+                #that's why we have 's' after dump
+                #when this converts dic object to string , it will be converted to json format
+
+with open("/home/mayurshimpi/Desktop/python_practice/Python_Practice/json/book.txt","w") as f:
+    f.write(s)
+
