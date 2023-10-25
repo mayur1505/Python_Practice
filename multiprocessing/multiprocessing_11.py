1de175ea841181f000312e689b8b1afe31e26fe9
Mayur Shimpi <mayurshimpi2014@gmail.com>
2023-10-25 23:09:41 +0530

    new change

--- multiprocessing/multiprocessing_11.py
+++ multiprocessing/multiprocessing_11.py
@@ -6,7 +6,7 @@ def f(n):
     return n*n
 
 if __name__ == "__main__":
-    p = Pool(processes=3)#at a time only 3 process will be created 
+    p = Pool(processes=3)#processes=3 means that the pool will have three worker processes/cores that can execute tasks in parallel
     result = p.map(f,[1,2,3,4,5])
     for n in result:
         print(n)
